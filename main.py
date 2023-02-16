552d05f4b992c1fc89c74472cabfa3e828dd632a
Manvir Mann <mmann@Manvirs-MacBook-Air.local>
2023-02-16 01:13:25 -0800

    Import fingerprint dataset and display image

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import cv2
+
+sample = cv2.imread("SOCOFing/Altered/Altered-Hard/150__M_Right_index_finger_Obl.BMP")
+# Resize fingerprint image
+sample = cv2.resize(sample, None, fx=2.5, fy=2.5)
+
+cv2.imshow("Sample", sample)
+# Keep image showing
+cv2.waitKey(0)
+cv2.destroyAllWindows()
