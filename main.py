871445571e95e152b75a2d5df2e48228c52fbf7e
Manvir Mann <manvirm29@gmail.com>
2023-02-16 01:21:55 -0800

    Import fingerprint dataset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import cv2
-
+# Import images
 sample = cv2.imread("SOCOFing/Altered/Altered-Hard/150__M_Right_index_finger_Obl.BMP")
 # Resize fingerprint image
 sample = cv2.resize(sample, None, fx=2.5, fy=2.5)
