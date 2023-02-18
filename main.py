03701673c03bb6a0040e336214670b58bf6f7f70
Manvir Mann <manvirm29@gmail.com>
2023-02-17 18:27:18 -0800

    Find best match using nearest neighbors

--- main.py
+++ main.py
@@ -11,12 +11,47 @@ image = None
 # Keypoints on sample and original
 kp1, kp2, mp = None, None, None
 
+counter = 0
 # Get images from real dataset
-for file in [file for file in os.listdir("SOCOFing/Real")][:1000]:
+for file in [file for file in os.listdir("SOCOFing/Real")]:
+    if counter % 10 == 0:
+        print(counter)
+    counter += 1
     fingerprint_image = cv2.imread("SOCOFing/Real/" + file)
 
     # Extract key points from images
-    sift = cv2.SIFT_create
+    sift = cv2.SIFT_create()
 
     keypoints_1, descriptors_1 = sift.detectAndCompute(sample, None)
     keypoints_2, descriptors_2 = sift.detectAndCompute(fingerprint_image, None)
+
+    # Find matches for keypoints (nearest neighbors)
+    matches = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10},
+                                    {}).knnMatch(descriptors_1, descriptors_2, k=2)
+    match_points = []
+
+    # Find relevant matches
+    for p, q in matches:
+        if p.distance < 0.1 * q.distance:
+            match_points.append(p)
+
+    keypoints = 0
+    if len(keypoints_1) < len(keypoints_2):
+        keypoints = len(keypoints_1)
+    else:
+        keypoints = len(keypoints_2)
+
+    if len(match_points) / keypoints * 100 > best_score:
+        best_score = len(match_points) / keypoints * 100
+        filename = file
+        image = fingerprint_image
+        kp1, kp2, mp = keypoints_1, keypoints_2, match_points
+
+print("BEST MATCH: " + filename)
+print("SCORE: " + str(best_score))
+
+result = cv2.drawMatches(sample, kp1, image, kp2, mp, None)
+result = cv2.resize(result, None, fx=4, fy=4)
+cv2.imshow("Result", result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
